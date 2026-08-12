89be6ae63d80e3618d43a4d715e3aa19f4b6bde3
claudialu0720 <claudialu0720@gmail.com>
2026-08-12 12:19:59 +0800

    Add google analytics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,22 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang={HTML_LANG}>
+      <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-R3W306V138"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-R3W306V138');
+            `,
+          }}
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
